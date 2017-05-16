ea22a5fbbd00f4a8a054147ff36192d1e8d6865e
Travis Mair <travis.mair@gmail.com>
2017-05-16 10:35:09 -0600

    Sanitize input and log message if path does not exist

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 component-inator
 =========
 
-  Simple tool to build a react-component skeleton.  It accepts two arguments: The Component name and the path where you want the component skeleton to be created.  The second argument must be a relative path from the directory it is running in. If the second argument is omitted, it will default to ./src/app/components/ 
+  Simple tool to build a react-component skeleton.  It accepts two arguments: The Component name and the path where you want the component skeleton to be created.  The second argument must be a relative path from the directory it is running in. If the second argument is omitted, it will default to /src/app/components/ relative to the root directory of the project.  Note: if the path does not exit, it will not run.
 
   The Skeleton assumes that you are using the @kadira/storybook module and creates storybook.js files needed for it.
 
--- index.js
+++ index.js
@@ -1,37 +1,43 @@
 const fs = require ('fs');
 const path = require ('path');
+const sanitize = require ('sanitize-filename');
 const {
   jsCreator,
   storyCreator,
   packageJSON,
   stylesCSS,
 } = require ('./templates');
+
+//check for arguments and log a message if none provided
 if (process.argv.length < 3) {
   console.log ('You must provide a component name');
 } else {
-  const component = process.argv[2];
+  const component = sanitize (process.argv[2]);
 
-  const dir = process.argv.length > 3
-    ? path.resolve (`${process.cwd ()}/${process.argv[3]}/${component}`)
-    : path.resolve (`${__dirname}/../../src/app/components/${component}`);
+  const parentDir = process.argv.length > 3
+    ? path.resolve (`${process.cwd ()}/${process.argv[3]}`)
+    : path.resolve (`${__dirname}/../../src/app/components`);
 
-  //check if directory exists
-  if (!fs.existsSync (dir)) {
-    //create path if it doesn't exist'
-    dir.split ('/').reduce ((path, folder) => {
-      path += folder + '/';
-      if (!fs.existsSync (path)) {
-        fs.mkdirSync (path);
-      }
-      return path;
-    }, '');
+  if (fs.existsSync (parentDir)) {
+    const dir = `/${component}`;
 
-    //create files from templates
-    fs.writeFileSync (`${dir}/${component}.js`, jsCreator (component));
-    fs.writeFileSync (`${dir}/${component}.story.js`, storyCreator (component));
-    fs.writeFileSync (`${dir}/package.json`, packageJSON (component));
-    fs.writeFileSync (`${dir}/style.css`, stylesCSS ());
+    //check if directory exists and log a message if it does
+    if (!fs.existsSync (dir)) {
+      //create path if it doesn't exist'
+      fs.mkdirSync (dir);
+
+      //create files from templates
+      fs.writeFileSync (`${dir}/${component}.js`, jsCreator (component));
+      fs.writeFileSync (
+        `${dir}/${component}.story.js`,
+        storyCreator (component)
+      );
+      fs.writeFileSync (`${dir}/package.json`, packageJSON (component));
+      fs.writeFileSync (`${dir}/style.css`, stylesCSS ());
+    } else {
+      console.log (`${dir} already exists.`);
+    }
   } else {
-    console.log (`${dir} already exists.`);
+    console.log (`${parentDir} does not exist.`);
   }
 }
