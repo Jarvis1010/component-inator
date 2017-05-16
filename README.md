ea00918e1bfc1a41b7570886621ad789873ca902
Travis Mair <travis.mair@gmail.com>
2017-05-15 20:24:25 -0600

    Check for args and make path arg relative to current working directory

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 component-inator
 =========
 
-Simple tool to build a react-component skeleton
+  Simple tool to build a react-component skeleton.  It accepts two arguments: The Component name and the path where you want the component skeleton to be created.  The second argument must be a relative path from the directory it is running in. If the second argument is omitted, it will default to ./src/app/components/ 
+
+  The Skeleton assumes that you are using the @kadira/storybook module and creates storybook.js files needed for it.
 
 ## Installation
 
-  npm install component-inator --save
+  npm install https://github.com/Jarvis1010/component-inator --save
 
 ## Usage
 
@@ -13,7 +15,7 @@ Simple tool to build a react-component skeleton
 
 ## Contributing
 
-In lieu of a formal styleguide, take care to format code using 'prettier' code formating tool prior to commit.
+In lieu of a formal styleguide, please format your code using the 'prettier' formating tool prior to commit.
 
 ## Release History
 
--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 const fs = require ('fs');
+const path = require ('path');
 const {
   jsCreator,
   storyCreator,
   packageJSON,
   stylesCSS,
 } = require ('./templates');
-
-const component = process.argv[2];
-
-const dir =
-  process.argv[3] || `${__dirname}/../../src/app/components/${component}`;
-
-if (!fs.existsSync (dir)) {
-  fs.mkdirSync (dir);
-  fs.writeFileSync (`${dir}/${component}.js`, jsCreator (component));
-  fs.writeFileSync (`${dir}/${component}.story.js`, storyCreator (component));
-  fs.writeFileSync (`${dir}/package.json`, packageJSON (component));
-  fs.writeFileSync (`${dir}/style.css`, stylesCSS ());
+if (process.argv.length < 3) {
+  console.log ('You must provide a component name');
 } else {
-  console.log (`${dir} already exists.`);
+  const component = process.argv[2];
+
+  const dir =
+    process.argv[3] ||
+    path.resolve (`${__dirname}/../../src/app/components/${component}`);
+
+  //check if directory exists
+  if (!fs.existsSync (dir)) {
+    //create path if it doesn't exist'
+    dir.split ('/').reduce ((path, folder) => {
+      path += folder + '/';
+      if (!fs.existsSync (path)) {
+        fs.mkdirSync (path);
+      }
+      return path;
+    }, '');
+
+    //create files from templates
+    fs.writeFileSync (`${dir}/${component}.js`, jsCreator (component));
+    fs.writeFileSync (`${dir}/${component}.story.js`, storyCreator (component));
+    fs.writeFileSync (`${dir}/package.json`, packageJSON (component));
+    fs.writeFileSync (`${dir}/style.css`, stylesCSS ());
+  } else {
+    console.log (`${dir} already exists.`);
+  }
 }
