47b91e191a6ed1dc8dca78026ced5c16407c3ae5
Travis Mair <travis.mair@gmail.com>
2017-06-05 14:32:25 -0600

    Correct container directory structure

--- create.js
+++ create.js
@@ -1,4 +1,4 @@
-const fs = require ('fs');
+const fs = require('fs');
 const {
   jsCreator,
   storyCreator,
@@ -6,36 +6,33 @@ const {
   stylesCSS,
   viewJS,
   containerJS,
-} = require ('./templates');
+} = require('./templates');
 
 module.exports.createComponent = (dir, component) => {
   //make directory
-  fs.mkdirSync (dir);
+  fs.mkdirSync(dir);
 
   //create files from templates
-  fs.writeFileSync (`${dir}/${component}.js`, jsCreator (component));
-  fs.writeFileSync (`${dir}/${component}.story.js`, storyCreator (component));
-  fs.writeFileSync (`${dir}/package.json`, packageJSON (component));
-  fs.writeFileSync (`${dir}/style.css`, stylesCSS ());
-  console.log (`${component} successfully created at ${dir}`);
+  fs.writeFileSync(`${dir}/${component}.js`, jsCreator(component));
+  fs.writeFileSync(`${dir}/${component}.story.js`, storyCreator(component));
+  fs.writeFileSync(`${dir}/package.json`, packageJSON(component));
+  fs.writeFileSync(`${dir}/style.css`, stylesCSS());
+  console.log(`${component} successfully created at ${dir}`);
 };
 
 module.exports.createView = (dir, component) => {
   //make directory
-  fs.mkdirSync (dir);
+  fs.mkdirSync(dir);
 
   //create files from templates
-  fs.writeFileSync (`${dir}/${component}.js`, viewJS (component));
-  fs.writeFileSync (`${dir}/package.json`, packageJSON (component));
-  fs.writeFileSync (`${dir}/style.css`, stylesCSS ());
-  console.log (`${component} successfully created at ${dir}`);
+  fs.writeFileSync(`${dir}/${component}.js`, viewJS(component));
+  fs.writeFileSync(`${dir}/package.json`, packageJSON(component));
+  fs.writeFileSync(`${dir}/style.css`, stylesCSS());
+  console.log(`${component} successfully created at ${dir}`);
 };
 
 module.exports.createContainer = (dir, component) => {
-  //make directory
-  fs.mkdirSync (dir);
-
   //create files from templates
-  fs.writeFileSync (`${dir}/${component}.js`, containerJS (component));
-  console.log (`${component} successfully created at ${dir}`);
+  fs.writeFileSync(`${dir}.js`, containerJS(component));
+  console.log(`${component} successfully created at ${dir}`);
 };
